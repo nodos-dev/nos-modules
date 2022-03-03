5db5f1358a0a3a4ff9caff0b63ff7c7b1aacd1b8
M. Samil Atesoglu <samil.atesoglu@zerodensity.tv>
2022-03-03 15:57:31 +0300

    Add basic composite graph

---
+++ MZMath/MathMain.cpp
@@ -0,0 +1,20 @@
+
+
+extern "C"
+{
+    void EntryPointAdder(/*AdderNode* adderNode*/)
+    {
+        // TODO
+        // float in1 = adderNode->in_1();
+        // float in2 = adderNode->in_2();
+        // adderNode->set_output(in1 + in2);
+    }
+
+    void EntryPointMultiplier(/*MultiplierNode* multiplierNode*/)
+    {
+        // TODO
+        float in1 = multiplierNode->in_1();
+        // float in2 = multiplierNode->in_2();
+        // multiplierNode->set_output(in1 * in2);
+    }
+}
