1cc27c10b757a9d767e6d924c7db38446143fa39
atkurtul <atilkurtulmus@gmail.com>
2022-06-26 08:09:16 +0300

    add make and cast vec

--- MZMath/MathMain.cpp
+++ MZMath/MathMain.cpp
@@ -20,7 +20,7 @@ void mzMath_API FAdd(size_t szIn, void* in, void* out)
 
 void mzMath_API __stdcall FMul(size_t szIn, void* in, void* out)
 {
-    **(float**)out = FOp<float, [](float a, float b) { return a+b; }>(szIn, (float**)in, 1);
+    **(float**)out = FOp<float, [](float a, float b) { return a*b; }>(szIn, (float**)in, 1);
 }
 
 void mzMath_API __stdcall FSub(size_t szIn, void* in, void* out)
@@ -31,6 +31,20 @@ void mzMath_API __stdcall FSub(size_t szIn, void* in, void* out)
 void mzMath_API __stdcall FDiv(size_t szIn, void* in, void* out)
 {
     **(float**)out = FOp<float, [](float a, float b) { return a/b; }>(szIn, (float**)in, 1);
+}
+
+void mzMath_API __stdcall MakeVec3(size_t szIn, void* in, void* out)
+{
+    ((float**)out)[0][0] = *((float**)in)[0];
+    ((float**)out)[0][1] = *((float**)in)[1];
+    ((float**)out)[0][2] = *((float**)in)[2];
+}
+
+void mzMath_API __stdcall CvtVec3ToVec3d(size_t szIn, void* in, void* out)
+{
+    ((double**)out)[0][0] = ((float**)in)[0][0];
+    ((double**)out)[0][1] = ((float**)in)[0][1];
+    ((double**)out)[0][2] = ((float**)in)[0][2];
 }
 
 }
