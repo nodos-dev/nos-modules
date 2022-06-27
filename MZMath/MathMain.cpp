9ac48482e3f47f1f20a7e109ed630e834e680465
atkurtul <atilkurtulmus@gmail.com>
2022-06-27 03:23:46 +0300

    templates

--- MZMath/MathMain.cpp
+++ MZMath/MathMain.cpp
@@ -36,10 +36,26 @@ struct vec
     friend vec operator/(vec l, vec r) { for(u32 i = 0; i < N; ++i) l[i] /= r[i]; return l;}
 };
 
+
+#define VEC_POSTFIX_i32 i
+#define VEC_POSTFIX_i64 i64
+#define VEC_POSTFIX_u32 u
+#define VEC_POSTFIX_u64 u64
+#define VEC_POSTFIX_f32 
+#define VEC_POSTFIX_f64 d
+
+#define EXPAND(...) __VA_ARGS__
+
+#define CONCAT(X,Y)      X##Y
+#define CONCAT4(X,Y,Z,W) X##Y##Z##W
+#define VEC_NAME_0(N, P) EXPAND(CONCAT(vec##N, P))
+#define VEC_NAME(T, N)   EXPAND(VEC_NAME_0(N, VEC_POSTFIX_##T))
+
+
 #define TYPEDEF_VEC(T) \
-using T##x2 = vec<T,2>;\
-using T##x3 = vec<T,3>;\
-using T##x4 = vec<T,4>;
+    using VEC_NAME(T,2) = vec<T,2>;\
+    using VEC_NAME(T,3) = vec<T,3>;\
+    using VEC_NAME(T,4) = vec<T,4>;
 
 TYPEDEF_VEC(i32)
 TYPEDEF_VEC(i64)
@@ -52,72 +68,77 @@ template<class T, T(Op)(T,T)>
 T TOp(u64 szIn, T** in, T identity)
 {
     for(size_t i = 0; i < szIn; ++i) 
-        identity = Op(*in[i], identity);
+    {
+        T lhs = *in[i];
+        identity = Op(lhs, identity);
+    }
     return identity;
 }
 
-#define DO_FOR_ALL_SCALARS(MACRO) \
-        MACRO(i32)                \
-        MACRO(i64)                \
-        MACRO(u32)                \
-        MACRO(u64)                \
-        MACRO(f32)                \
-        MACRO(f64)
-
-#define GENERATE_OP(F, T, I, Op) void mzMath_API F(size_t szIn, T** in, T** out) { **out = TOp<T, [](T a, T b) -> T { return a Op b; }>(szIn, in, I);}
-#define GENERATE_CVT(from, to)   void mzMath_API Cast_##from##_To_##to(size_t, from** in, to** out) { **out = **in; };
-#define MAKE_VEC(T, N)           void mzMath_API MakeVec_##T##x##N(size_t szIn, T** in, vec<T,N>** out) { **out = vec<T,N>::Load(in); }
+#define GENERATE_OP(F, T, I, Op) void mzMath_API F(size_t szIn, void* in, void* out) { **(T**)out = TOp<T, [](T a, T b) -> T { return a Op b; }>(szIn, (T**)in, I);}
+#define GENERATE_CVT(from, to)   void mzMath_API CONCAT4(Cast_,from,_To_,to)(size_t, from** in, to** out) { **out = **in; };
+#define MAKE_VEC(T,N,V)          void mzMath_API CONCAT(MakeVec_,V)(size_t szIn, T** in, vec<T,N>** out) { **out = vec<T,N>::Load(in); }
 
 #define GENERATE_OP_ALL(T)           \
-    GENERATE_OP(Add_##T, T, T(0), +) \
-    GENERATE_OP(Sub_##T, T, T(0), -) \
-    GENERATE_OP(Mul_##T, T, T(1), *) \
-    GENERATE_OP(Div_##T, T, T(1), /) 
+    EXPAND(GENERATE_OP(CONCAT(Add_, T), T, T(0), +)) \
+    EXPAND(GENERATE_OP(CONCAT(Sub_, T), T, T(0), -)) \
+    EXPAND(GENERATE_OP(CONCAT(Mul_, T), T, T(1), *)) \
+    EXPAND(GENERATE_OP(CONCAT(Div_, T), T, T(1), /)) 
 
 #define GENERATE_OP_ALL_BATCH(T) \
-    GENERATE_OP_ALL(T)           \
-    GENERATE_OP_ALL(T##x2)       \
-    GENERATE_OP_ALL(T##x3)       \
-    GENERATE_OP_ALL(T##x4)
+    EXPAND(GENERATE_OP_ALL(T))   \
+    EXPAND(GENERATE_OP_ALL(VEC_NAME(T,2))) \
+    EXPAND(GENERATE_OP_ALL(VEC_NAME(T,3))) \
+    EXPAND(GENERATE_OP_ALL(VEC_NAME(T,4)))
 
-#define GENERATE_CVT_ALL(from, to)  \
-    GENERATE_CVT(from, to)          \
-    GENERATE_CVT(from##x2, to##x2)  \
-    GENERATE_CVT(from##x3, to##x3)  \
-    GENERATE_CVT(from##x4, to##x4)
+#define GENERATE_CVT_ALL_0(from, to) EXPAND(GENERATE_CVT(from, to))
+#define GENERATE_CVT_ALL(from, to) \
+    EXPAND(GENERATE_CVT_ALL_0(from, to)) \
+    EXPAND(GENERATE_CVT_ALL_0(VEC_NAME(from, 2), VEC_NAME(to, 2))) \
+    EXPAND(GENERATE_CVT_ALL_0(VEC_NAME(from, 3), VEC_NAME(to, 3))) \
+    EXPAND(GENERATE_CVT_ALL_0(VEC_NAME(from, 4), VEC_NAME(to, 4)))
 
 #define GENERATE_CVT_ALL_CROSS      \
-        GENERATE_CVT_ALL(i32, i64)  \
-        GENERATE_CVT_ALL(i32, u32)  \
-        GENERATE_CVT_ALL(i32, u64)  \
-        GENERATE_CVT_ALL(i32, f32)  \
-        GENERATE_CVT_ALL(i32, f64)  \
-        GENERATE_CVT_ALL(i64, i32)  \
-        GENERATE_CVT_ALL(i64, u32)  \
-        GENERATE_CVT_ALL(i64, u64)  \
-        GENERATE_CVT_ALL(i64, f32)  \
-        GENERATE_CVT_ALL(i64, f64)  \
-        GENERATE_CVT_ALL(f32, i32)  \
-        GENERATE_CVT_ALL(f32, i64)  \
-        GENERATE_CVT_ALL(f32, u32)  \
-        GENERATE_CVT_ALL(f32, u64)  \
-        GENERATE_CVT_ALL(f32, f64)  \
-        GENERATE_CVT_ALL(f64, i32)  \
-        GENERATE_CVT_ALL(f64, i64)  \
-        GENERATE_CVT_ALL(f64, u32)  \
-        GENERATE_CVT_ALL(f64, u64)  \
-        GENERATE_CVT_ALL(f64, f32) 
+        EXPAND(GENERATE_CVT_ALL(i32, i64))  \
+        EXPAND(GENERATE_CVT_ALL(i32, u32))  \
+        EXPAND(GENERATE_CVT_ALL(i32, u64))  \
+        EXPAND(GENERATE_CVT_ALL(i32, f32))  \
+        EXPAND(GENERATE_CVT_ALL(i32, f64))  \
+        EXPAND(GENERATE_CVT_ALL(i64, i32))  \
+        EXPAND(GENERATE_CVT_ALL(i64, u32))  \
+        EXPAND(GENERATE_CVT_ALL(i64, u64))  \
+        EXPAND(GENERATE_CVT_ALL(i64, f32))  \
+        EXPAND(GENERATE_CVT_ALL(i64, f64))  \
+        EXPAND(GENERATE_CVT_ALL(f32, i32))  \
+        EXPAND(GENERATE_CVT_ALL(f32, i64))  \
+        EXPAND(GENERATE_CVT_ALL(f32, u32))  \
+        EXPAND(GENERATE_CVT_ALL(f32, u64))  \
+        EXPAND(GENERATE_CVT_ALL(f32, f64))  \
+        EXPAND(GENERATE_CVT_ALL(f64, i32))  \
+        EXPAND(GENERATE_CVT_ALL(f64, i64))  \
+        EXPAND(GENERATE_CVT_ALL(f64, u32))  \
+        EXPAND(GENERATE_CVT_ALL(f64, u64))  \
+        EXPAND(GENERATE_CVT_ALL(f64, f32)) 
 
 #define MAKE_VEC_ALL(T) \
-        MAKE_VEC(T, 2)  \
-        MAKE_VEC(T, 3)  \
-        MAKE_VEC(T, 4)  \
+        EXPAND(MAKE_VEC(T, 2, VEC_NAME(T, 2))) \
+        EXPAND(MAKE_VEC(T, 3, VEC_NAME(T, 3))) \
+        EXPAND(MAKE_VEC(T, 4, VEC_NAME(T, 4)))
+
+#define ALL_GENERATORS(T)  \
+    EXPAND(GENERATE_OP_ALL_BATCH(T)) \
+    EXPAND(MAKE_VEC_ALL(T))
+
 
 extern "C"
 {
 
-DO_FOR_ALL_SCALARS(GENERATE_OP_ALL_BATCH)
-DO_FOR_ALL_SCALARS(MAKE_VEC_ALL)
 GENERATE_CVT_ALL_CROSS
+ALL_GENERATORS(i32)
+ALL_GENERATORS(i64)
+ALL_GENERATORS(u32)
+ALL_GENERATORS(u64)
+ALL_GENERATORS(f32)
+ALL_GENERATORS(f64)
 
 }
