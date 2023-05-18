8f62beb687928011659803951cfea91dbe3623b7
M. Samil Atesoglu <samil.atesoglu@mediaz.io>
2023-05-18 19:05:02 +0300

    WIP: Filters new API
    Co-authored-by: mtuncbilek <metehan.tuncbilek@mediaz.io>

---
+++ mzFilters/Source/FiltersMain.cpp
@@ -0,0 +1,73 @@
+// Copyright MediaZ AS. All Rights Reserved.
+
+#include <MediaZ/PluginAPI.h>
+#include <MediaZ/Helpers.hpp>
+#include <Builtins_generated.h>
+
+#include <glm/glm.hpp>
+
+// Shaders
+#include "Color.frag.spv.dat"
+#include "ColorCorrect.frag.spv.dat"
+#include "Diff_frag_spv.frag.spv.dat"
+
+MZ_INIT();
+
+namespace mz::filters
+{
+
+enum Filters
+{
+    Color = 0,
+    ColorCorrect,
+    Diff,
+    Count
+};
+
+#define COLOR_PIN_COLOR_IDX 0
+#define COLOR_PIN_OUTPUT_IDX 1
+
+extern "C"
+{
+
+mzPluginSDK_API int mzPluginSDK_CALL mzGetNodeTypeCount()
+{
+    return Filters::Count;
+}
+
+mzPluginSDK_API MzResult mzPluginSDK_CALL mzExportNodeFunctions(int nodeTypeIndex, MzNodeFunctions* outFunctions)
+{
+    switch (nodeTypeIndex){
+        case Filters::Color:
+        {
+            outFunctions->TypeName = "mz.Color";
+            outFunctions->GetShaderSource = [](MzBuffer* outSpirvBuf) -> MzResult {
+                outSpirvBuf->Data = Color_frag_spv;
+                outSpirvBuf->Size = sizeof(Color_frag_spv);
+                return MzResult::Success;
+            }
+        }
+        case Filters::ColorCorrect:
+        {
+            outFunctions->TypeName = "mz.ColorCorrect";
+            outFunctions->GetShaderSource = [](MzBuffer* outSpirvBuf) -> MzResult {
+                outSpirvBuf->Data = ColorCorrect_frag_spv;
+                outSpirvBuf->Size = sizeof(ColorCorrect_frag_spv);
+                return MzResult::Success;
+            }
+        }
+        case Filters::Diff:
+        {
+            outFunctions->TypeName = "mz.Diff";
+            outFunctions->GetShaderSource = outFunctions->GetShaderSource = [](MzBuffer* outSpirvBuf) -> MzResult {
+                outSpirvBuf->Data = Diff_frag_spv;
+                outSpirvBuf->Size = sizeof(Diff_frag_spv);
+                return MzResult::Success;
+            }
+        }
+        default:
+            return MzResult::InvalidArgument;
+    }
+}
+}
+}
