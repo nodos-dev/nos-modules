4ef0a033f1b0862c67e042cbce7f4d662b89d2e6
Caner <caner.coskun@mediaz.io>
2023-11-27 18:56:53 +0300

    Rename mediaz to nodos - part 1

--- Subsystems/mzVulkanSubsystem/Source/Globals.cpp
+++
@@ -1,9 +0,0 @@
-#include "Globals.h"
-#include "Resources/ResourceManager.h"
-
-namespace mz::vkss
-{
-mz::vk::Device* GVkDevice;
-vk::rc<mz::vk::Context> GVkCtx;
-std::unique_ptr<ResourceManager> GResources{};
-}
