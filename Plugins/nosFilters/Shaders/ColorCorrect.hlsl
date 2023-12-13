de75aeb7ccf065b207cdb9578fc5ccd686aea427
atkurtul <atilkurtulmus@gmail.com>
2023-12-13 17:51:10 +0300

    hlsl support

---
+++ Plugins/nosFilters/Shaders/ColorCorrect.hlsl
@@ -0,0 +1,48 @@
+
+struct Input
+{
+[[vk::location(0)]] float2 uv : UV0;
+};
+
+
+SamplerState StaticSampler {
+    Filter = MIN_MAG_MIP_LINEAR;
+    AddressU = Wrap;
+    AddressV = Wrap;
+};
+
+[[vk::binding(0, 0)]] Texture2D<float4> Source;
+
+
+[[vk::binding(1, 0)]] 
+cbuffer ubo { 
+    float4 Saturation;
+    float4 Contrast;
+    float4 Gamma;
+    float4 Gain;
+    float4 Offset;
+    float3 ContrastCenter;
+};
+
+template<class T>
+T mult(T v)
+{
+    return v * T(4, 1, 0.5);
+}
+
+float3 Correct(float3 WorkingColor, float4 Saturation, float4 Contrast, float4 Gamma, float4 Gain, float4 Offset, float3 ContrastCenter)
+{
+	float Luma = dot(WorkingColor, float3(0.2722287168,0.6740817658,0.0536895174));
+	WorkingColor = max(float3(0,0,0), lerp(Luma.xxx, WorkingColor, Saturation.xyz * Saturation.w));
+	WorkingColor = pow(WorkingColor * (1.0 / ContrastCenter), Contrast.xyz * Contrast.w) * ContrastCenter;
+	WorkingColor = pow(WorkingColor, 1.0 / (Gamma.xyz * Gamma.w));
+	WorkingColor = WorkingColor * (Gain.xyz * Gain.w) + Offset.xyz + Offset.w;
+	return mult(WorkingColor);
+}
+
+
+float4 main(Input input) : SV_TARGET0
+{
+    float4 src = Source.Sample(StaticSampler, input.uv);
+    return float4(Correct(src.rgb, Saturation, Contrast, Gamma, Gain, Offset, ContrastCenter), src.a);
+}
