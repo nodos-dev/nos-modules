eb01607a05fa2bbb9bb3409cd74629684b5a79f8
Caner <caner.coskun@nodos.dev>
2023-12-13 13:06:43 +0300

    Add logs to TestNode events

--- Plugins/nosTest/Source/Test.cpp
+++ Plugins/nosTest/Source/Test.cpp
@@ -14,27 +14,96 @@ NOS_REGISTER_NAME(in1)
 NOS_REGISTER_NAME(in2)
 NOS_REGISTER_NAME(out)
 
-
-static void TestFunction(void* ctx, const nosNodeExecuteArgs* nodeArgs, const nosNodeExecuteArgs* functionArgs)
+class TestNode : public nos::NodeContext
 {
-	auto args = nos::GetPinValues(functionArgs);
-
-	auto a = *GetPinValue<double>(args, NSN_in1);
-	auto b = *GetPinValue<double>(args, NSN_in2);
-	auto c = a + b;
-	nosEngine.SetPinValue(functionArgs->Pins[2].Id, {.Data = &c, .Size = sizeof(c)});
-}
-
-static nosResult GetFunctions(size_t* outCount, nosName* pName, nosPfnNodeFunctionExecute* fns)
-{
-	*outCount = 1;
-	if (!pName || !fns)
+public:
+	TestNode(const nosFbNode* node) : nos::NodeContext(node) 
+	{ 
+		nosEngine.LogI("TestNode: " __FUNCTION__);
+	}
+	~TestNode() 
+	{ 
+		nosEngine.LogI("TestNode: " __FUNCTION__);
+	}
+	virtual void OnNodeUpdated(const nosFbNode* updatedNode) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+	virtual void OnPinValueChanged(nos::Name pinName, nosUUID pinId, nosBuffer value)
+	{
+		nosEngine.LogI("TestNode: " __FUNCTION__);
+	}
+	virtual void OnPinConnected(nos::Name pinName, nosUUID connectedPin) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+	virtual void OnPinDisconnected(nos::Name pinName) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+	virtual void OnPinShowAsChanged(nos::Name pinName, nos::fb::ShowAs showAs)
+	{
+		nosEngine.LogI("TestNode: " __FUNCTION__);
+	}
+	virtual void OnPathCommand(const nosPathCommand* command) { nosEngine.LogI("TestNode: OnNodeUpdated"); }
+	virtual nosResult CanRemoveOrphanPin(nos::Name pinName, nosUUID pinId)
+	{
+		nosEngine.LogI("TestNode: " __FUNCTION__);
 		return NOS_RESULT_SUCCESS;
+	}
+	virtual nosResult OnOrphanPinRemoved(nos::Name pinName, nosUUID pinId)
+	{
+		nosEngine.LogI("TestNode: " __FUNCTION__);
+		return NOS_RESULT_SUCCESS;
+	}
 
-	*fns = TestFunction;
-	*pName = NOS_NAME_STATIC("TestFunction");
-	return NOS_RESULT_SUCCESS;
-}
+	// Execution
+	virtual nosResult ExecuteNode(const nosNodeExecuteArgs* args)
+	{
+		nosEngine.LogI("TestNode: " __FUNCTION__);
+		return NOS_RESULT_SUCCESS;
+	}
+	virtual nosResult CanCopy(nosCopyInfo* copyInfo)
+	{
+		nosEngine.LogI("TestNode: " __FUNCTION__);
+		return NOS_RESULT_SUCCESS;
+	}
+	virtual nosResult BeginCopyFrom(nosCopyInfo* copyInfo)
+	{
+		nosEngine.LogI("TestNode: " __FUNCTION__);
+		return NOS_RESULT_SUCCESS;
+	}
+	virtual nosResult BeginCopyTo(nosCopyInfo* copyInfo)
+	{
+		nosEngine.LogI("TestNode: " __FUNCTION__);
+		return NOS_RESULT_SUCCESS;
+	}
+	virtual void EndCopyFrom(nosCopyInfo* copyInfo) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+	virtual void EndCopyTo(nosCopyInfo* copyInfo) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+
+	// Menu & key events
+	virtual void OnMenuRequested(const nosContextMenuRequest* request) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+	virtual void OnMenuCommand(nosUUID itemID, uint32_t cmd) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+	virtual void OnKeyEvent(const nosKeyEvent* keyEvent) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+
+	virtual void OnPinDirtied(nosUUID pinID, uint64_t frameCount) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+	virtual void OnPathStateChanged(nosPathState pathState) { nosEngine.LogI("TestNode: " __FUNCTION__); }
+
+	
+	static void TestFunction(void* ctx, const nosNodeExecuteArgs* nodeArgs, const nosNodeExecuteArgs* functionArgs)
+	{
+		auto args = nos::GetPinValues(functionArgs);
+
+		auto a = *GetPinValue<double>(args, NSN_in1);
+		auto b = *GetPinValue<double>(args, NSN_in2);
+		auto c = a + b;
+		nosEngine.SetPinValue(functionArgs->Pins[2].Id, {.Data = &c, .Size = sizeof(c)});
+	}
+
+	static nosResult GetFunctions(size_t* outCount, nosName* pName, nosPfnNodeFunctionExecute* fns)
+	{
+		*outCount = 1;
+		if (!pName || !fns)
+			return NOS_RESULT_SUCCESS;
+
+		*fns = TestFunction;
+		*pName = NOS_NAME_STATIC("TestFunction");
+		return NOS_RESULT_SUCCESS;
+	}
+};
+
+
 
 
 extern "C"
@@ -51,8 +120,7 @@ extern "C"
 		if (ret != NOS_RESULT_SUCCESS)
 			return ret;
 		
-		outFunctions[0]->ClassName = NOS_NAME_STATIC("nos.test.NodeTest");
-		outFunctions[0]->GetFunctions = GetFunctions;
+		NOS_BIND_NODE_CLASS(NOS_NAME_STATIC("nos.test.NodeTest"), TestNode, outFunctions[0]);
 		outFunctions[1]->ClassName = NOS_NAME_STATIC("nos.test.NodeWithCategories");
 		outFunctions[2]->ClassName = NOS_NAME_STATIC("nos.test.NodeWithFunctions");
 		outFunctions[3]->ClassName = NOS_NAME_STATIC("nos.test.NodeWithCustomTypes");
