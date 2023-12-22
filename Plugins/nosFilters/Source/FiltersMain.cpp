534905e5aef35694af895ab96c3ec6e5cf18a416
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2023-12-22 18:06:51 +0300

    Remove unnecessarily exporting GPU node functions in nos.filters

--- Plugins/nosFilters/Source/FiltersMain.cpp
+++ Plugins/nosFilters/Source/FiltersMain.cpp
@@ -3,19 +3,6 @@
 // Includes
 #include <Nodos/PluginAPI.h>
 #include <glm/glm.hpp>
-#include <Builtins_generated.h>
-
-// Shaders
-#include "../Shaders/ColorCorrect.frag.spv.dat"
-#include "../Shaders/Diff.frag.spv.dat"
-#include "../Shaders/GaussianBlur.frag.spv.dat"
-#include "../Shaders/Kuwahara.frag.spv.dat"
-#include "../Shaders/KawaseLightStreak.frag.spv.dat"
-#include "../Shaders/PremultiplyAlpha.frag.spv.dat"
-#include "../Shaders/Sharpen.frag.spv.dat"
-#include "../Shaders/Sobel.frag.spv.dat"
-#include "../Shaders/Thresholder.frag.spv.dat"
-#include "../Shaders/Sampler.frag.spv.dat"
 
 // Nodes
 #include "GaussianBlur.hpp"
@@ -27,21 +14,9 @@ namespace nos::filters
 
 enum Filters : int
 {
-	ColorCorrect = 0,
-	Diff,
-	Kuwahara,
-	GaussianBlur,
-	KawaseLightStreak,
-	PremultiplyAlpha,
-	Sharpen,
-	Sobel,
-	Thresholder,
-	Sampler,
+	GaussianBlur = 0,
 	Count
 };
-
-#define COLOR_PIN_COLOR_IDX 0
-#define COLOR_PIN_OUTPUT_IDX 1
 
 extern "C"
 {
@@ -53,30 +28,7 @@ NOSAPI_ATTR nosResult NOSAPI_CALL nosExportNodeFunctions(size_t* outSize, nosNod
 		*outSize = Filters::Count;
 		return NOS_RESULT_SUCCESS;
 	}
-
-#define REGISTER_NODE(NODE) \
-	outList[Filters::NODE]->ClassName = NOS_NAME_STATIC("nos.filters." #NODE); \
-	
-#define REGISTER_NODE_LICENSED(NODE, featureName, featureMessage)								\
-		REGISTER_NODE(NODE)																		\
-		outList[Filters::NODE]->OnNodeCreated = [](const nosFbNode* node, void** outCtxPtr) {	\
-		nosEngine.RegisterFeature(*node->id(), featureName, 1, featureMessage);					\
-		};																						\
-		outList[Filters::NODE]->OnNodeDeleted = [](void* ctx, nosUUID nodeId) {					\
-		nosEngine.UnregisterFeature(nodeId, featureName);										\
-		};
-
-	REGISTER_NODE(ColorCorrect);
-	REGISTER_NODE(Diff);
-	REGISTER_NODE(Kuwahara);
 	RegisterGaussianBlur(outList[GaussianBlur]);
-	REGISTER_NODE(KawaseLightStreak);
-	REGISTER_NODE(PremultiplyAlpha);
-	REGISTER_NODE(Sharpen);
-	REGISTER_NODE(Sobel);
-	REGISTER_NODE(Thresholder);
-	REGISTER_NODE(Sampler);
-	
 	return NOS_RESULT_SUCCESS;
 }
 }
