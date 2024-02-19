ab93f37b37dde6b4051c0385df857f40aea3447e
bertaye <bertayeren@gmail.com>
2024-02-19 18:16:07 +0300

    Prepare onnx outputs, make onnx runner to check connections

--- Plugins/nosONNX/Source/ONNXRegisterer.cpp
+++ Plugins/nosONNX/Source/ONNXRegisterer.cpp
@@ -4,9 +4,11 @@
 #include <AppService_generated.h>
 #include <AppEvents_generated.h>
 #include "nosAI/nosAISubsystem.h"
+#include "nosTensorSubsystem/nosTensorSubsystem.h"
 
 NOS_INIT();
 void RegisterONNXRunner(nosNodeFunctions* outFunctions);
+
 extern nosAISubsystem* nosAI = nullptr;
 extern nosTensorSubsystem* nosTensor = nullptr;
 extern "C"
@@ -25,7 +27,7 @@ extern "C"
 		returnRes = nosEngine.RequestSubsystem(NOS_NAME_STATIC(NOS_TENSOR_SUBSYSTEM_NAME), 1, 0, (void**)&nosTensor);
 		if (returnRes != NOS_RESULT_SUCCESS)
 			return NOS_RESULT_FAILED;
-
+		
 		RegisterONNXRunner(outFunctions[0]);
 
 		return NOS_RESULT_SUCCESS;
--- Plugins/nosONNX/Source/ONNXRunner.cpp
+++ Plugins/nosONNX/Source/ONNXRunner.cpp
@@ -3,8 +3,9 @@
 #include <Nodos/PluginHelpers.hpp>
 #include <AppService_generated.h>
 #include <AppEvents_generated.h>
-#include "nosAI/nosAISubsystem.h"
-#include "flatbuffers/flatbuffers.h"
+#include <nosTensorSubsystem/TensorTypes_generated.h>
+#include <nosAI/nosAISubsystem.h>
+#include <flatbuffers/flatbuffers.h>
 #include "ORT_generated.h"
 
 NOS_REGISTER_NAME(ONNXRunner);
@@ -16,6 +17,13 @@ struct ONNXRunnerNodeContext : nos::NodeContext
 {
 	nosUUID NodeUUID, InputUUID, OutputUUID;
 	ONNXModel Model;
+	std::condition_variable RunModel_CV;
+	std::mutex RunModelMutex;
+	std::thread ModelRunnerThread;
+
+	nosTensorInfo InputTensor = {}, OutputTensor = {};
+	nosCUDABufferInfo OutputTensorCUDABuffer = {};
+	bool IsFixedOutput = true;
 	ONNXRunnerNodeContext(nos::fb::Node const* node) :NodeContext(node) {
 		NodeUUID = *node->id();
 	}
@@ -23,13 +31,62 @@ struct ONNXRunnerNodeContext : nos::NodeContext
 	~ONNXRunnerNodeContext() {
 	}
 
+	virtual void OnPinValueChanged(nos::Name pinName, nosUUID pinId, nosBuffer value) {
+		if (InputUUID == pinId) {
+			if (IsFixedOutput)
+				return;
+			if (OutputTensor.CreateInfo.ShapeInfo.Dimensions != NULL) {
+				auto tensor = flatbuffers::GetRoot<nos::sys::tensor::Tensor>(value.Data);
+				for (int i = 0; i < OutputTensor.CreateInfo.ShapeInfo.DimensionCount; i++) {
+					if (OutputTensor.CreateInfo.ShapeInfo.Dimensions[i] == -1) {
+						//THIS IS NOT ROBUST AT ALL, MUST HELD A BETTER ASSUMPTION
+						OutputTensor.CreateInfo.ShapeInfo.Dimensions[i] = tensor->shape()->Get(i);
+					}
+				}
+				PrepareOutput();
+			}
+			
+
+		}
+	}
+
+	void PrepareOutput() {
+		if (OutputTensor.CreateInfo.Location != MEMORY_LOCATION_CUDA)
+			return;
+
+		if (OutputTensorCUDABuffer.CreateInfo.AllocationSize == OutputTensor.MemoryInfo.Size) {
+			return;
+		}
+
+		for (int i = 0; i < OutputTensor.CreateInfo.ShapeInfo.DimensionCount; i++) {
+			if (OutputTensor.CreateInfo.ShapeInfo.Dimensions[i] == -1) {
+				return;
+			}
+		}
+
+		if (OutputTensorCUDABuffer.Address != NULL) {
+			nosCUDA->DestroyBuffer(&OutputTensorCUDABuffer);
+		}
+		nosCUDA->CreateShareableBufferOnCUDA(&OutputTensorCUDABuffer, OutputTensor.MemoryInfo.Size);
+		
+		nosTensor->ImportTensorFromCUDABuffer(&OutputTensor, &OutputTensorCUDABuffer, OutputTensor.CreateInfo.ShapeInfo, OutputTensor.CreateInfo.ElementType);
+
+		TensorPinConfig outputCfg = { .Name = "Output Tensor", .ShowAs = TENSOR_SHOW_AS_OUTPUT_PIN, .CanShowAs = TENSOR_CAN_SHOW_AS_OUTPUT_PIN_OR_PROPERTY };
+		nosTensor->UpdateTensorPin(&OutputTensor, &NodeUUID, &OutputUUID, outputCfg);
+	}
+
+	nosResult ExecuteNode(const nosNodeExecuteArgs* args) override {
+
+		return NOS_RESULT_SUCCESS; 
+	}
+
+
 	void RunModel() {
 
 	}
 
 	nosResult LoadModel(std::filesystem::path modelPath, bool isReload = false) {
 
-		ONNXModel mnistModel;
 		ONNXLoadConfig loadConfig = {};
 		loadConfig.OptimizationLevel = OPTIMIZATION_ENABLE_ALL;
 		loadConfig.RunLocation = RUN_ONNX_ON_CUDA;
@@ -40,23 +97,49 @@ struct ONNXRunnerNodeContext : nos::NodeContext
 		ONNXCUDAOptions cudaOptions = { .DeviceID = 0 };
 		loadConfig.CUDAOptions = &cudaOptions;
 
-		nosResult res = nosAI->LoadONNXModel(&mnistModel, modelPath.string().c_str(), loadConfig);
-		nosTensorInfo inputTensor = {};
-		memcpy(&inputTensor.CreateInfo.ShapeInfo, &mnistModel.Inputs->Shape, sizeof(nosTensorShapeInfo));
-		inputTensor.CreateInfo.Location = MEMORY_LOCATION_CUDA;
-		inputTensor.CreateInfo.ElementType = mnistModel.Inputs[0].ElementType;
+		nosResult res = nosAI->LoadONNXModel(&Model, modelPath.string().c_str(), loadConfig);
+		memcpy(&InputTensor.CreateInfo.ShapeInfo, &Model.Inputs->Shape, sizeof(nosTensorShapeInfo));
+		InputTensor.CreateInfo.Location = MEMORY_LOCATION_CUDA;
+		//TODO: Query for multiple inputs / outputs!!!
+		InputTensor.CreateInfo.ElementType = Model.Inputs[0].ElementType;
 
-		nosTensorInfo outputTensor = {};
-		memcpy(&outputTensor.CreateInfo.ShapeInfo, &mnistModel.Outputs->Shape, sizeof(nosTensorShapeInfo));
-		outputTensor.CreateInfo.Location = MEMORY_LOCATION_CUDA;
-		outputTensor.CreateInfo.ElementType = mnistModel.Outputs[0].ElementType;
+		memcpy(&OutputTensor.CreateInfo.ShapeInfo, &Model.Outputs->Shape, sizeof(nosTensorShapeInfo));
+		OutputTensor.CreateInfo.Location = MEMORY_LOCATION_CUDA;
+		OutputTensor.CreateInfo.ElementType = Model.Outputs[0].ElementType;
+		IsFixedOutput = true;
+		for (int i = 0; i < OutputTensor.CreateInfo.ShapeInfo.DimensionCount; i++) {
+			if (OutputTensor.CreateInfo.ShapeInfo.Dimensions[i] == -1) {
+				IsFixedOutput = false;
+			}
+		}
 
 		TensorPinConfig inputCfg = { .Name = "Input Tensor", .ShowAs = TENSOR_SHOW_AS_INPUT_PIN, .CanShowAs = TENSOR_CAN_SHOW_AS_INPUT_PIN_OR_PROPERTY };
 		TensorPinConfig outputCfg = { .Name = "Output Tensor", .ShowAs = TENSOR_SHOW_AS_OUTPUT_PIN, .CanShowAs = TENSOR_CAN_SHOW_AS_OUTPUT_PIN_OR_PROPERTY };
-		nosTensor->CreateTensorPin(&inputTensor, &NodeUUID, &InputUUID, inputCfg);
-		nosTensor->CreateTensorPin(&outputTensor, &NodeUUID, &OutputUUID, outputCfg);
-
+		nosTensor->UpdateTensorPin(&InputTensor, &NodeUUID, &InputUUID, inputCfg);
+		nosTensor->UpdateTensorPin(&OutputTensor, &NodeUUID, &OutputUUID, outputCfg);
+		PrepareOutput();
 		return NOS_RESULT_SUCCESS;
+	}
+
+	static nosBool CanConnectPin(void* ctx, nosName pinName, nosUUID connectedPinId, const nosBuffer* connectedPinData) {
+		ONNXRunnerNodeContext* node = reinterpret_cast<ONNXRunnerNodeContext*>(ctx);
+		const nos::sys::tensor::Tensor* connectedTensor = flatbuffers::GetRoot<nos::sys::tensor::Tensor>(connectedPinData->Data);
+		nos::sys::tensor::TensorElementDataType ConnectedElementType = connectedTensor->element_type();
+		if (ConnectedElementType != (nos::sys::tensor::TensorElementDataType)node->InputTensor.CreateInfo.ElementType) {
+			return NOS_FALSE;
+		}
+
+		if (connectedTensor->shape()->size() != node->InputTensor.CreateInfo.ShapeInfo.DimensionCount) { 
+			return NOS_FALSE; 
+		}
+
+		for (int i = 0; i < node->InputTensor.CreateInfo.ShapeInfo.DimensionCount; i++) {
+			if (node->InputTensor.CreateInfo.ShapeInfo.Dimensions[i]!=-1 && connectedTensor->shape()->Get(i) != node->InputTensor.CreateInfo.ShapeInfo.Dimensions[i]) {
+				return NOS_FALSE;
+			}
+		}
+
+		return NOS_TRUE;
 	}
 
 	static nosResult GetFunctions(size_t* count, nosName* names, nosPfnNodeFunctionExecute* fns)
@@ -82,5 +165,7 @@ struct ONNXRunnerNodeContext : nos::NodeContext
 };
 
 void RegisterONNXRunner(nosNodeFunctions* outFunctions) {
+	outFunctions->CanConnectPin = ONNXRunnerNodeContext::CanConnectPin;
 	NOS_BIND_NODE_CLASS(NSN_ONNXRunner, ONNXRunnerNodeContext, outFunctions);
 }
+
