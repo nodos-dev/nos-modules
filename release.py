238475f9efe99c7298d5e07a937e32a65a39671d
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-03-19 14:40:51 +0300

    CD: Pull releases repo before commit

--- release.py
+++ release.py
@@ -243,6 +243,11 @@ def upload_releases(repo_url, org_name, repo_name, cloned_release_repo, dry_run)
             logger.error(f"Failed to set git user name: {re.stderr}")
             exit(re.returncode)
 
+        # Update repo
+        re = custom_run(["git", "pull"], dry_run)
+        if re.returncode != 0:
+            logger.error(f"Failed to pull: {re.stderr}")
+            exit(re.returncode)
         # Commit the result and create a release
         re = custom_run(["git", "add", f"{module_name}/index.json"], dry_run)
         if re.returncode != 0:
@@ -253,6 +258,9 @@ def upload_releases(repo_url, org_name, repo_name, cloned_release_repo, dry_run)
             logger.error(f"Failed to commit index file: {re.stderr}")
             exit(re.returncode)
         re = custom_run(["git", "push"], dry_run)
+        if re.returncode != 0:
+            logger.error(f"Failed to push: {re.stderr}")
+            exit(re.returncode)
 
         os.chdir("..")
 
