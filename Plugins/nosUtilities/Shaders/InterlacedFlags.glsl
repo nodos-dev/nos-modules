eed9d73770b088a3700bca719065beb220402134
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-03-20 15:21:06 +0300

    Add missing InterlacedFlags.glsl

---
+++ Plugins/nosUtilities/Shaders/InterlacedFlags.glsl
@@ -0,0 +1,16 @@
+/*
+ * Copyright MediaZ AS. All Rights Reserved.
+ */
+
+#ifndef _INTERLACED_FLAGS_H
+#define _INTERLACED_FLAGS_H
+
+#define NOS_OUTPUT_EVEN_FIELD_MASK 1
+#define NOS_OUTPUT_ODD_FIELD_MASK 2
+#define NOS_OUTPUT_INTERLACED_MASK 3
+#define NOS_INPUT_EVEN_FIELD_MASK 4
+#define NOS_INPUT_ODD_FIELD_MASK 8
+#define NOS_INPUT_INTERLACED_MASK 12
+#define NOS_COMP10_MASK 16
+
+#endif
