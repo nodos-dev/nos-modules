ab7eee4b6a2ce50bc5ecaa5b943bae26eadfb632
Caner <caner.coskun@nodos.dev>
2024-04-05 18:13:05 +0300

    AJA: Delete context menu code from channel node

--- Plugins/nosAJA/Source/ChannelNode.cpp
+++ Plugins/nosAJA/Source/ChannelNode.cpp
@@ -173,81 +173,6 @@ struct ChannelNodeContext : NodeContext
  		CurrentChannel.Update(std::move(channelPin), true);
 	}
 
-	void OnPinMenuRequested(nos::Name pinName, const nosContextMenuRequest* request) override
-	{
-	}
-
-	void OnNodeMenuRequested(const nosContextMenuRequest* request) override
-	{
-		flatbuffers::FlatBufferBuilder fbb;
-		std::vector<flatbuffers::Offset<nos::ContextMenuItem>> items, out;
-		EnumerateOutputChannels(fbb, out);
-		if (!out.empty())
-			items.push_back(nos::CreateContextMenuItemDirect(fbb, "Open Output Channel", 0, &out));
-		std::vector<flatbuffers::Offset<nos::ContextMenuItem>> in;
-		EnumerateInputChannels(fbb, in);
-		if (!in.empty())
-			items.push_back(nos::CreateContextMenuItemDirect(fbb, "Open Input Channel", 0, &in));
-		if (!items.empty())
-			HandleEvent(CreateAppEvent(fbb, nos::app::CreateAppContextMenuUpdateDirect(fbb, &NodeId, request->pos(), request->instigator(), &items)));
-	}
-
-	void OnMenuCommand(nosUUID itemID, uint32_t cmd) override
-	{
-	    if (!cmd)
-	        return;
-
-	    AJASelectChannelCommand action = reinterpret_cast<AJASelectChannelCommand&>(cmd);
-		auto device = AJADevice::GetDevice(action.DeviceIndex);
-	    auto channel = action.Channel;
-		auto input = action.Input;
-        const bool isQuad = action.IsQuad;
-        const AJADevice::Mode mode = isQuad ? (input ? AJADevice::AUTO : AJADevice::TSI) : AJADevice::SL;
-        NTV2VideoFormat format = action.Format;
-        if (input)
-            format = device->GetInputVideoFormat(action.Channel);
-
-        u32 width = 1920 * (1 + isQuad);
-        u32 height = 1080 * (1 + isQuad);
-        device->GetExtent(format, mode, width, height);
-
-	    std::string channelName = (isQuad ? GetQuadName(channel) : ("SingleLink " + std::to_string(channel + 1)));
-		{
-			TChannelInfo channelPin{};
-			channelPin.device = std::make_unique<TDevice>(TDevice{ {}, device->GetSerialNumber(), device->GetDisplayName() });
-			channelPin.channel_name = channelName;
-			channelPin.is_input = input;
-	    	channelPin.is_quad = isQuad;
-			channelPin.video_format = NTV2VideoFormatToString(format, true); // TODO: Readonly.
-			channelPin.video_format_idx = static_cast<int>(format);
-			if (isQuad)
-				channelPin.output_quad_link_mode = static_cast<QuadLinkMode>(mode);
-			channelPin.frame_buffer_format = static_cast<utilities::YCbCrPixelFormat>(CurrentPixelFormat);
-			CurrentChannel.Update(std::move(channelPin), true);
-		}
-		flatbuffers::FlatBufferBuilder fbb;
-	    std::vector<flatbuffers::Offset<fb::Pin>> pins;
-		std::vector<fb::UUID> pinsToDelete;
-		{ // Quad mode setting
-	    	if (isQuad && !QuadLinkModePinId)
-	    	{
-	    		std::vector<u8> quadLinkModeData = nos::Buffer::From(mode);
-	    		auto id = GenerateUUID();
-	    		pins.push_back(nos::fb::CreatePinDirect(
-					fbb, &id, (channelName + " Mode").c_str(), input ? "nos.aja.QuadLinkInputMode" : "nos.aja.QuadLinkMode",
-					nos::fb::ShowAs::PROPERTY, nos::fb::CanShowAs::PROPERTY_ONLY, 0, 0, &quadLinkModeData));
-	    		QuadLinkModePinId = id;
-	    	}
-	    	else if (!isQuad && QuadLinkModePinId)
-	    	{
-	    		pinsToDelete.push_back(*QuadLinkModePinId);
-	    		QuadLinkModePinId = std::nullopt;
-	    	}
-		}
-		if (!pins.empty())
-			HandleEvent(CreateAppEvent(fbb, nos::CreatePartialNodeUpdateDirect(fbb, &NodeId, ClearFlags::NONE, &pinsToDelete, &pins)));
-	}
-
 	void UpdateVisualizer(nos::Name pinName, std::string stringListName)
 	{ 
 		SetPinVisualizer(pinName, {.type = nos::fb::VisualizerType::COMBO_BOX, .name = stringListName});
@@ -325,20 +250,6 @@ struct ChannelNodeContext : NodeContext
 	void ResetInterlacedPin()
 	{ 
 		SetPinValue(NSN_IsInterlaced, nosBuffer{.Data = (void*)"NONE", .Size = 5});
-	}
-
-	std::string GetFirstDeviceName()
-	{
-		AJADevice* outDev;
-		if (AJADevice::GetAvailableDevice(IsInput, &outDev))
-		{
-			return outDev->GetDisplayName();
-		}
-		else if (AJADevice::Devices.size() > 0)
-		{
-			return AJADevice::Devices.begin()->second->GetDisplayName();
-		}
-		return "NONE";
 	}
 
 	std::string GetDeviceStringListName() { return "aja.DeviceList." + UUID2STR(NodeId); }
