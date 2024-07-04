bcf581d50fd6fc79d5482d41e9240a9ba2d1316b
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-07-04 18:59:43 +0300

    Add test code to test subsystem

--- Subsystems/nosTestSubsystem/Source/Test.cpp
+++ Subsystems/nosTestSubsystem/Source/Test.cpp
@@ -1,8 +1,16 @@
 // Copyright MediaZ Teknoloji A.S. All Rights Reserved.
 #include <nosTestSubsystem/TestSubsystem.h>
 #include <Nodos/SubsystemAPI.h>
+#include <nosVulkanSubsystem/nosVulkanSubsystem.h>
+#include <Nodos/Helpers.hpp>
+NOS_INIT_WITH_MIN_REQUIRED_MINOR(7); // APITransition
+NOS_VULKAN_INIT();
 
-NOS_INIT();
+void OnRequestedSubsystemUnloaded(nosName name, int versionMajor, int versionMinor)
+{
+	if (name == NOS_NAME(NOS_VULKAN_SUBSYSTEM_NAME))
+		nosVulkan = nullptr;	
+}
 
 extern "C"
 {
@@ -18,6 +26,15 @@ NOSAPI_ATTR nosResult NOSAPI_CALL OnRequest(uint32_t minor, void** outSubsystemC
 NOSAPI_ATTR nosResult NOSAPI_CALL nosExportSubsystem(nosSubsystemFunctions* subsystemFunctions)
 {
 	subsystemFunctions->OnRequest = OnRequest;
+	subsystemFunctions->OnRequestedSubsystemUnloaded = OnRequestedSubsystemUnloaded;
+	auto ret = RequestVulkanSubsystem();
+	if (ret != NOS_RESULT_SUCCESS)
+		return ret;
+	return NOS_RESULT_SUCCESS;
+}
+
+NOSAPI_ATTR nosResult NOSAPI_CALL nosUnloadSubsystem()
+{
 	return NOS_RESULT_SUCCESS;
 }
 
