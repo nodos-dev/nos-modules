401fc6a0da5efc2839cfae361ff19601a6d06450
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-09-03 12:27:43 +0300

    Make GaussianBlur node a graph with two directional blurs

--- Plugins/nosFilters/Source/GaussianBlur.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright MediaZ Teknoloji A.S. All Rights Reserved.
-
-#pragma once
-
-#include <Nodos/Helpers.hpp>
-#include <Nodos/PluginAPI.h>
-
-void RegisterGaussianBlur(nosNodeFunctions* out);
