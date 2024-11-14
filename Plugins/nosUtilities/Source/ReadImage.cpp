a2c073133dcb1eeff20ad7a30338aead654645fe
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2024-11-14 15:16:33 +0300

    Fix ReadImage flicker after image load
    Create image resource before loading and set output pin data after load completed

--- Plugins/nosUtilities/Source/ReadImage.cpp
+++ Plugins/nosUtilities/Source/ReadImage.cpp
@@ -37,17 +37,30 @@ enum State
     Failed = 2,
 };
 
-struct ReadImageContext
+struct ReadImageContext : NodeContext
 {
-    std::atomic<State> state;
-    decltype(Clock::now()) time_started;
-	fb::UUID nodeid;
-	std::string load_path;
+    std::atomic<State> CurrentState;
+    decltype(Clock::now()) TimeStarted;
+	std::filesystem::path FilePath;
 
+	std::mutex OutImageDecRefCallbacksMutex;
+	std::queue<std::function<void()>> OutImageDecRefCallbacks;
+
+	ReadImageContext(nosFbNode const* node) : 
+		NodeContext(node), 
+		CurrentState(State::Idle), 
+		TimeStarted(Clock::now())
+	{
+	}
+
+	~ReadImageContext()
+	{
+		FlushImageDecRefCallbacks();
+	}
 
 	void UpdateStatus(State newState)
 	{
-        if(newState == state.exchange(newState))
+        if(newState == CurrentState.exchange(newState))
         {
             return;
         }
@@ -57,16 +70,15 @@ struct ReadImageContext
         switch(newState)
         {
         case State::Loading:
-		    time_started = Clock::now();
+		    TimeStarted = Clock::now();
 			msg.push_back(fb::CreateNodeStatusMessageDirect(fbb, "Loading image", fb::NodeStatusMessageType::INFO));
             break;
         case State::Idle:
         {
 			std::stringstream ss;
-			auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(Clock::now() - time_started);
-            ss << "Read Image: Loaded in " << dt << "\nFile: " << load_path;
-            nosEngine.LogDI(load_path.c_str(), ss.str().c_str());
-			msg.push_back(fb::CreateNodeStatusMessageDirect(fbb, "Loaded image", fb::NodeStatusMessageType::INFO));
+			auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(Clock::now() - TimeStarted).count();
+			auto statusText = std::string("Image ") + FilePath.filename().string() + " loaded in " + std::to_string(dt) + "ms";
+			msg.push_back(fb::CreateNodeStatusMessageDirect(fbb, statusText.c_str(), fb::NodeStatusMessageType::INFO));
             break;
         }
         case State::Failed:
@@ -74,32 +86,40 @@ struct ReadImageContext
             break;
         }
 
-        HandleEvent(CreateAppEvent(fbb, nos::CreatePartialNodeUpdateDirect(fbb, &nodeid, ClearFlags::NONE, 0, 0, 0, 0, 0, 0, &msg)));
+        HandleEvent(CreateAppEvent(fbb, nos::CreatePartialNodeUpdateDirect(fbb, &NodeId, ClearFlags::NONE, 0, 0, 0, 0, 0, 0, &msg)));
 	}
-};
 
-static nosResult GetFunctions(size_t* count, nosName* names, nosPfnNodeFunctionExecute* fns)
-{
-    *count = 1;
-    if(!names || !fns)
-        return NOS_RESULT_SUCCESS;
-    
-    *names = NOS_NAME_STATIC("ReadImage_Load");
-    
-	*fns = [](void* ctx, nosFunctionExecuteParams* params)
-    {
-        auto c = (ReadImageContext*)ctx;
-        if(c->state == State::Loading)
-        {
-            nosEngine.LogE("Read Image is already loading an image.");
-            return NOS_RESULT_FAILED;
-        }
+	void FlushImageDecRefCallbacks()
+	{
+		std::lock_guard<std::mutex> lock(OutImageDecRefCallbacksMutex);
+		while (!OutImageDecRefCallbacks.empty())
+		{
+			OutImageDecRefCallbacks.front()();
+			OutImageDecRefCallbacks.pop();
+		}
+	}
 
-        c->UpdateStatus(State::Loading);
+	static nosResult OnImageLoaded(void* ctx, nosFunctionExecuteParams* params)
+	{
+		auto c = (ReadImageContext*)ctx;
+		c->FlushImageDecRefCallbacks();
+		return NOS_RESULT_SUCCESS;
+	}
+	
+	static nosResult Load(void* ctx, nosFunctionExecuteParams* params)
+	{
+		auto c = (ReadImageContext*)ctx;
+		if (c->CurrentState == State::Loading)
+		{
+			nosEngine.LogE("Read Image is already loading an image.");
+			return NOS_RESULT_FAILED;
+		}
+
+		c->UpdateStatus(State::Loading);
 
 		nos::NodeExecuteParams nodeParams(params->ParentNodeExecuteParams);
 		std::filesystem::path path = InterpretPinValue<const char>(nodeParams[NSN_Path].Data->Data);
-		c->load_path = path.string();
+		c->FilePath = path.string();
 		try
 		{
 			if (!std::filesystem::exists(path))
@@ -117,19 +137,19 @@ static nosResult GetFunctions(size_t* count, nosName* names, nosPfnNodeFunctionE
 				c->UpdateStatus(State::Failed);
 				return NOS_RESULT_FAILED;
 			}
-			
+
 			nosResourceShareInfo outRes = {
 				.Info = {.Type = NOS_RESOURCE_TYPE_TEXTURE,
-						 .Texture = {.Width = (u32)w, .Height = (u32)h, .Format = NOS_FORMAT_R8G8B8A8_UNORM, .FieldType = NOS_TEXTURE_FIELD_TYPE_PROGRESSIVE}}};
+							.Texture = {.Width = (u32)w, .Height = (u32)h, .Format = NOS_FORMAT_R8G8B8A8_UNORM, .FieldType = NOS_TEXTURE_FIELD_TYPE_PROGRESSIVE}} };
 
 			// unless reading raw bytes, this is useless since samplers convert to linear space automatically
 			if (*InterpretPinValue<bool>(nodeParams[NSN_sRGB].Data->Data))
 				outRes.Info.Texture.Format = NOS_FORMAT_R8G8B8A8_SRGB;
 
-			nosEngine.SetPinValue(nodeParams[NSN_Out].Id, nos::Buffer::From(nos::vkss::ConvertTextureInfo(outRes)));
-			std::thread([c, nodeParams = vkss::OwnedNodeExecuteParams(*params->ParentNodeExecuteParams), &outRes]() mutable  {
-				nos::NodeExecuteParams params(&nodeParams);
-				std::filesystem::path path = InterpretPinValue<const char>(params[NSN_Path].Data->Data);
+			std::filesystem::path path = InterpretPinValue<const char>(nodeParams[NSN_Path].Data->Data);
+			auto outPinId = nodeParams[NSN_Out].Id;
+
+			std::thread([c, outPinId, path, outRes]() mutable {
 				try
 				{
 					if (!std::filesystem::exists(path))
@@ -146,52 +166,68 @@ static nosResult GetFunctions(size_t* count, nosName* names, nosPfnNodeFunctionE
 						return;
 					}
 
-					auto res = vkss::DeserializeTextureInfo(params[NSN_Out].Data->Data);
+					nosVulkan->CreateResource(&outRes);
+					nosVulkan->SetResourceTag(&outRes, "ReadImage Texture");
 
 					nosCmd cmd{};
-					nosVulkan->Begin("ReadImage: Load", &cmd);
-					nosVulkan->ImageLoad(cmd, img, nosVec2u(w, h), NOS_FORMAT_R8G8B8A8_SRGB, &res);
-					nosCmdEndParams endParams{.ForceSubmit = true};
+					nosCmdBeginParams beginParams {
+						.Name = NOS_NAME("ReadImage Load"),
+						.AssociatedNodeId = c->NodeId,
+						.OutCmdHandle = &cmd
+					};
+					nosVulkan->Begin2(&beginParams);
+					nosVulkan->ImageLoad(cmd, img, nosVec2u(w, h), NOS_FORMAT_R8G8B8A8_SRGB, &outRes);
+					nosCmdEndParams endParams{ .ForceSubmit = true };
 					nosVulkan->End(cmd, &endParams);
 
+					nosEngine.SetPinValue(outPinId, nos::Buffer::From(vkss::ConvertTextureInfo(outRes)));
+
+					{
+						std::lock_guard<std::mutex> lock(c->OutImageDecRefCallbacksMutex);
+						c->OutImageDecRefCallbacks.push([c, outRes]() {
+							nosVulkan->DestroyResource(&outRes);
+						});
+					}
+					
+					nosEngine.CallNodeFunction(c->NodeId, NOS_NAME_STATIC("OnImageLoaded"));
+
 					free(img);
-					flatbuffers::FlatBufferBuilder fbb;
-					HandleEvent(CreateAppEvent(fbb, app::CreatePinDirtied(fbb, &params[NSN_Out].Id)));
 				}
 				catch (const std::exception& e)
 				{
 					nosEngine.LogE("Error while loading image: %s", e.what());
 				}
 
-                c->UpdateStatus(State::Idle);
+				c->UpdateStatus(State::Idle);
 			}).detach();
 		}
 		catch (const std::exception& e)
 		{
 			nosEngine.LogE("Error while loading image: %s", e.what());
-            c->UpdateStatus(State::Failed);
+			c->UpdateStatus(State::Failed);
 		}
 		return NOS_RESULT_SUCCESS;
-    };
-    
-    return NOS_RESULT_SUCCESS;
-}
+	}
 
+	static nosResult GetFunctions(size_t* count, nosName* names, nosPfnNodeFunctionExecute* fns)
+	{
+		*count = 2;
+		if (!names || !fns)
+			return NOS_RESULT_SUCCESS;
+
+		names[0] = NOS_NAME_STATIC("ReadImage_Load");
+		names[1] = NOS_NAME_STATIC("OnImageLoaded");
+
+		fns[0] = &Load;
+		fns[1] = &OnImageLoaded;
+
+		return NOS_RESULT_SUCCESS;
+	}
+};
 
 nosResult RegisterReadImage(nosNodeFunctions* fn)
 {
-	fn->ClassName = NSN_Nos_Utilities_ReadImage;
-	fn->GetFunctions = GetFunctions;
-
-	fn->OnNodeCreated = [](const nosFbNode* node, void** outCtxPtr)
-	{
-		*outCtxPtr = new ReadImageContext{.nodeid = *node->id()};
-	};
-	fn->OnNodeDeleted = [](void* ctx, nosUUID nodeId)
-	{
-		delete (ReadImageContext*)ctx;
-	};
-
+	NOS_BIND_NODE_CLASS(NSN_Nos_Utilities_ReadImage, ReadImageContext, fn);
 	return NOS_RESULT_SUCCESS;
 }
 
